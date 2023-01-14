5a49b0c7aeb7a1e91048fee1394047ad02b2777b
Adam <56907039+adamjsturge@users.noreply.github.com>
2023-01-13 19:57:05 -0600

    Fixes made after testing

--- easy-xss-hunter.sh
+++ easy-xss-hunter.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
 
-read -p "Do you want to install Docker? (y/n) " choice
-
 while true; do
-    read -p "Do you want to proceed? (y/n) " yn
+    read -p "Do you want to install Docker? (y/n) " yn
     case $yn in 
         [yY] )
             curl -fsSL get.docker.com -o get-docker.sh
             sudo sh get-docker.sh
             break;;
-        [nN] );;
+        [nN] )break;;
         * ) echo invalid response;;
     esac
 done
@@ -24,7 +22,7 @@ do
             break
             ;;
         "Slack Notification Fork (https://github.com/adamjsturge/xsshunter-express)")
-        SLACK_FORK = true
+        SLACK_FORK=true
             git clone https://github.com/adamjsturge/xsshunter-express.git
             break
             ;;
@@ -32,7 +30,7 @@ do
     esac
 done
 
-cd xsshunter-express
+cd xsshunter-express/
 
 read -p "Do you want to go through basic setup? (y/n) " yn
 case $yn in 
@@ -49,82 +47,82 @@ echo "Leave anything blank you don't know or don't want to change"
 
 read -p "What is your hostname? " HOSTNAME
 if [ -n "$HOSTNAME" ]; then
-    sed -i "s/HOSTNAME=your.host.name/HOSTNAME=$HOSTNAME/g" docker-compose.yml
+    sed -i "s/HOSTNAME=your.host.name/HOSTNAME=$HOSTNAME/" docker-compose.yml
 fi
 
 read -p "What is your SSL contact email? " SSL_CONTACT_EMAIL
 if [ -n "$SSL_CONTACT_EMAIL" ]; then
-    sed -i "s/SSL_CONTACT_EMAIL=YourEmail@gmail.com/SSL_CONTACT_EMAIL=$SSL_CONTACT_EMAIL/g" docker-compose.yml
+    sed -i "s/SSL_CONTACT_EMAIL=YourEmail@gmail.com/SSL_CONTACT_EMAIL=$SSL_CONTACT_EMAIL/" docker-compose.yml
 fi
 
 read -p "Do you want to enable SMTP email notifications? (y/n) " SMTP_EMAIL_NOTIFICATIONS_ENABLED
 if [[ "$SMTP_EMAIL_NOTIFICATIONS_ENABLED" =~ [Yy] ]]; then
-    sed -i "s/SMTP_EMAIL_NOTIFICATIONS_ENABLED=false/SMTP_EMAIL_NOTIFICATIONS_ENABLED=true/g" docker-compose.yml
+    sed -i "s/SMTP_EMAIL_NOTIFICATIONS_ENABLED=false/SMTP_EMAIL_NOTIFICATIONS_ENABLED=true/" docker-compose.yml
     read -p "What is your SMTP host? " SMTP_HOST
     if [ -n "$SMTP_HOST" ]; then
-        sed -i "s/SMTP_HOST=smtp.gmail.com/SMTP_HOST=$SMTP_HOST/g" docker-compose.yml
+        sed -i "s/SMTP_HOST=smtp.gmail.com/SMTP_HOST=$SMTP_HOST/" docker-compose.yml
     fi
 
     read -p "What is your SMTP port (default 465)? " SMTP_PORT
     if [ -n "$SMTP_PORT" ]; then
-        sed -i "s/SMTP_PORT=465/SMTP_PORT=$SMTP_PORT/g" docker-compose.yml
+        sed -i "s/SMTP_PORT=465/SMTP_PORT=$SMTP_PORT/" docker-compose.yml
     fi
 
     read -p "Do you want to enable SMTP use TLS? (y/n) " SMTP_USE_TLS
     if [[ "$SMTP_USE_TLS" =~ [Yy] ]]; then
-        sed -i "s/SMTP_USE_TLS=false/SMTP_USE_TLS=true/g" docker-compose.yml
+        sed -i "s/SMTP_USE_TLS=false/SMTP_USE_TLS=true/" docker-compose.yml
     else
-        sed -i "s/SMTP_USE_TLS=true/SMTP_USE_TLS=false/g" docker-compose.yml
+        sed -i "s/SMTP_USE_TLS=true/SMTP_USE_TLS=false/" docker-compose.yml
     fi
 
     read -p "What is your SMTP username? " SMTP_USERNAME
     if [ -n "$SMTP_USERNAME" ]; then
-        sed -i "s/SMTP_USERNAME=YourEmail@gmail.com/SMTP_USERNAME=$SMTP_USERNAME/g" docker-compose.yml
+        sed -i "s/SMTP_USERNAME=YourEmail@gmail.com/SMTP_USERNAME=$SMTP_USERNAME/" docker-compose.yml
     fi
 
     read -p "What is your SMTP password? " SMTP_PASSWORD
     if [ -n "$SMTP_PASSWORD" ]; then
-        sed -i "s/SMTP_PASSWORD=YourEmailPassword/SMTP_PASSWORD=$SMTP_PASSWORD/g" docker-compose.yml
+        sed -i "s/SMTP_PASSWORD=YourEmailPassword/SMTP_PASSWORD=$SMTP_PASSWORD/" docker-compose.yml
     fi
 
     read -p "What is your SMTP from email? " SMTP_FROM_EMAIL
     if [ -n "$SMTP_FROM_EMAIL" ]; then
-        sed -i "s/SMTP_FROM_EMAIL=YourEmail@gmail.com/SMTP_FROM_EMAIL=$SMTP_FROM_EMAIL/g" docker-compose.yml
+        sed -i "s/SMTP_FROM_EMAIL=YourEmail@gmail.com/SMTP_FROM_EMAIL=$SMTP_FROM_EMAIL/" docker-compose.yml
     fi
 
     read -p "What is your SMTP receiver email? " SMTP_RECEIVER_EMAIL
     if [ -n "$SMTP_RECEIVER_EMAIL" ]; then
-        sed -i "s/SMTP_RECEIVER_EMAIL=YourEmail@gmail.com/SMTP_RECEIVER_EMAIL=$SMTP_RECEIVER_EMAIL/g" docker-compose.yml
+        sed -i "s/SMTP_RECEIVER_EMAIL=YourEmail@gmail.com/SMTP_RECEIVER_EMAIL=$SMTP_RECEIVER_EMAIL/" docker-compose.yml
     fi
 
 else
-    sed -i "s/SMTP_EMAIL_NOTIFICATIONS_ENABLED=true/SMTP_EMAIL_NOTIFICATIONS_ENABLED=false/g" docker-compose.yml
+    sed -i "s/SMTP_EMAIL_NOTIFICATIONS_ENABLED=true/SMTP_EMAIL_NOTIFICATIONS_ENABLED=false/" docker-compose.yml
 fi
 
-if [ -n "SLACK_FORK "]; then
+if [ -n "$SLACK_FORK" ]; then
     read -p "Do you want to enable Slack notifications? (y/n) " SLACK_NOTIFICATIONS_ENABLED
     if [[ "$SLACK_NOTIFICATIONS_ENABLED" =~ [Yy] ]]; then
-        sed -i "s/SLACK_NOTIFICATIONS_ENABLED=false/SLACK_NOTIFICATIONS_ENABLED=true/g" docker-compose.yml
+        sed -i "s/SLACK_NOTIFICATIONS_ENABLED=false/SLACK_NOTIFICATIONS_ENABLED=true/" docker-compose.yml
         read -p "What is your Slack webhook? " SLACK_WEBHOOK
         if [ -n "$SLACK_WEBHOOK" ]; then
-            sed -i "s|SLACK_WEBHOOK=hooks.slack.com/services/|SLACK_WEBHOOK=$SLACK_WEBHOOK|g" docker-compose.yml
+            sed -i "s|SLACK_WEBHOOK=hooks.slack.com/services/|SLACK_WEBHOOK=$SLACK_WEBHOOK/" docker-compose.yml
         fi
 
         read -p "What is your Slack channel? " SLACK_CHANNEL
         if [ -n "$SLACK_CHANNEL" ]; then
-            sed -i "s/SLACK_CHANNEL=xssalerting/SLACK_CHANNEL=$SLACK_CHANNEL/g" docker-compose.yml
+            sed -i "s/SLACK_CHANNEL=xssalerting/SLACK_CHANNEL=$SLACK_CHANNEL/" docker-compose.yml
         fi
 
         read -p "What is your Slack username? " SLACK_USERNAME
         if [ -n "$SLACK_USERNAME" ]; then
-            sed -i "s/SLACK_USERNAME=XSS-Hunter/SLACK_USERNAME=$SLACK_USERNAME/g" docker-compose.yml
+            sed -i "s/SLACK_USERNAME=XSS-Hunter/SLACK_USERNAME=$SLACK_USERNAME/" docker-compose.yml
         fi
 
         read -p "What is your Slack emoji? " SLACK_EMOJI
         if [ -n "$SLACK_EMOJI" ]; then
-            sed -i "s/SLACK_EMOJI=hackerman/SLACK_EMOJI=$SLACK_EMOJI/g" docker-compose.yml
+            sed -i "s/SLACK_EMOJI=hackerman/SLACK_EMOJI=$SLACK_EMOJI/" docker-compose.yml
         fi
     else
-        sed -i "s/SLACK_NOTIFICATIONS_ENABLED=true/SLACK_NOTIFICATIONS_ENABLED=false/g" docker-compose.yml
+        sed -i "s/SLACK_NOTIFICATIONS_ENABLED=true/SLACK_NOTIFICATIONS_ENABLED=false/" docker-compose.yml
     fi
 fi
