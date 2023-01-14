edcd5f257b2118f5606dbc0c1cda3f4556f1383a
Adam <56907039+adamjsturge@users.noreply.github.com>
2023-01-13 19:04:56 -0600

    Initial Script

--- easy-xss-hunter.sh
+++ easy-xss-hunter.sh
@@ -1,1 +1,34 @@
+#!/bin/bash
 
+read -p "Do you want to install Docker? (y/n) " choice
+
+while true; do
+    read -p "Do you want to proceed? (y/n) " yn
+    case $yn in 
+        [yY] )
+            curl -fsSL get.docker.com -o get-docker.sh
+            sudo sh get-docker.sh
+            break;;
+        [nN] );;
+        * ) echo invalid response;;
+    esac
+done
+
+options=("Official Repo (https://github.com/mandatoryprogrammer/xsshunter-express)" "Slack Notification Fork (https://github.com/adamjsturge/xsshunter-express)")
+PS3='Which Github option would you like to choose? '
+select opt in "${options[@]}"
+do
+    case $opt in
+        "Official Repo (https://github.com/mandatoryprogrammer/xsshunter-express)")
+            git clone https://github.com/mandatoryprogrammer/xsshunter-express.git
+            break
+            ;;
+        "Slack Notification Fork (https://github.com/adamjsturge/xsshunter-express)")
+            git clone https://github.com/adamjsturge/xsshunter-express.git
+            break
+            ;;
+        *) echo "Invalid option. Please select again.";;
+    esac
+done
+
+cd xsshunter-express
